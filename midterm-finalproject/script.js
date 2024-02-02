90cb33edb84657ac42e14e3b9381941091c042da
JaquelineNavarro <155691208+JaquelineNavarro@users.noreply.github.com>
2024-02-01 22:16:03 -0800

    multiple html pages

---
+++ midterm-finalproject/script.js
@@ -0,0 +1,24 @@
+
+// function sendMail(){
+//     var params = {
+//         name: document.getElementById("name").value, 
+//         email: document.getElementById("email").value, 
+//         message: document.getElementById("message").value,
+//     }
+// const serviceID = "service_y6cwz0x";
+// const templateID ="template_5mlmggf";
+
+// emailjs.send(serviceID,templateID,params)
+// .then(
+//     res =>{
+//         document.getElementById("name").value = "";
+//         document.getElementById("email").value = "";
+//         document.getElementById("message").value = "";
+//         console.log(res);
+//         alert("Your message was sent!")
+//     }
+// )
+// .catch(err=console.log(err));
+// }
+<form action="name-of-your-thankyou-page.html" method="get"
+name="myContact"></form>
