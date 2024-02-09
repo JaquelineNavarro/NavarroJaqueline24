ccd3449a4c8f986579466a928168defe7ebee558
JaquelineNavarro <155691208+JaquelineNavarro@users.noreply.github.com>
2024-02-09 12:56:26 -0800

    changing styles

--- a3/script.js
+++ a3/script.js
@@ -7,6 +7,7 @@ function createRhombus(pHeight, pColorEven, pColorOdd, pSymbol) {
     upRight(pHeight, pColorEven, pColorOdd, pSymbol);
     downRight(pHeight, pColorEven, pColorOdd, pSymbol);
     downLeft (pHeight, pColorEven, pColorOdd, pSymbol);
+    upLeft(pHeight, pColorEven, pColorOdd, pSymbol)
 }
     
 function upRight(pHeight, pColorEven, pColorOdd, pSymbol) {
@@ -70,3 +71,23 @@ function downLeft(pHeight,pColorEven,pColorOdd,pSymbol) {
     document.getElementById("downLeft").innerHTML = lLine;
 }
 
+function upLeft(pHeight, pColorEven, pColorOdd, pSymbol) {
+    var lLine = "";
+    for (i=0;i<pHeight;i++) {
+        lLine +="<p>";
+        //Create each line on the Rhombus
+        for(j=0;j<=i;j++) {
+            //Is the position even or odd so we change the color
+            if (j%2)
+            //even
+                lLine +="<span style='color:" + pColorEven + ";'>" + pSymbol +"</span>";
+            else
+            //odd
+                lLine +="<span style='color:" + pColorOdd + ";'>" + pSymbol +"</span>";
+        }
+        lLine +="</p>";
+    console.log(lLine);
+    }
+
+    document.getElementById("upLeft").innerHTML = lLine;
+}
