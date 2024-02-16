3637e13d628ccb278b7e2afe6842eab240bd9f8b
JaquelineNavarro <155691208+JaquelineNavarro@users.noreply.github.com>
2024-02-15 18:37:05 -0800

    update

--- m07/script.js
+++ m07/script.js
@@ -6,14 +6,11 @@ function Hotel(name, rooms, booked) {
     this.checkAvailability = function() {
       return this.rooms - this.booked;
     };
-  }
-  
-  
+  }  
   // Create two hotel objects
   var quayHotel = new Hotel('Quay', 40, 25);
   var parkHotel = new Hotel('Park', 120, 77);
   var sunsetHotel = new Hotel('Sunset', 86, 10);
-  
   
   // Update the HTML for the page
   var details1 = quayHotel.name + ' rooms: ';
