d381fa86cf2c2b33a8e32e0c63ee911391bf9a41
JaquelineNavarro <155691208+JaquelineNavarro@users.noreply.github.com>
2024-03-07 20:06:04 -0800

    Captions of the images

--- a5/script.js
+++ a5/script.js
@@ -43,25 +43,25 @@ var playButton = document.getElementById("playbtn");
 playButton.addEventListener("click",setInterval)
 
 
-function playSlideShow () {
+// function playSlideShow () {
     
-}
-const images = document.querySelectorAll('img');
-let i = 0;
+// }
+// const images = document.querySelectorAll('img');
+// let i = 0;
 
-setInterval(function(){
-    if(i == 0) {
-        images[i].style.display = 'block';
-    } else if(i == images.length) {
-        images[i - 1].style.display = 'none';
-        images[0].style.display = 'block';
-        i = 0; 
-    } else {
-        images[i - 1].style.display = 'none';
-        images [i].style.display = 'block';
-    }
-    i ++;
-}, 3000);
+// setInterval(function(){
+//     if(i == 0) {
+//         images[i].style.display = 'block';
+//     } else if(i == images.length) {
+//         images[i - 1].style.display = 'none';
+//         images[0].style.display = 'block';
+//         i = 0; 
+//     } else {
+//         images[i - 1].style.display = 'none';
+//         images [i].style.display = 'block';
+//     }
+//     i ++;
+// }, 3000);
 
 
 
